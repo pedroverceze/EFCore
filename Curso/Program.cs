64fce42f71813534cf2c1a69ab9b8ca4a446e454
Pedro <pedroverceze@hotmail.com>
2022-02-02 17:30:36 -0300

    consultas com carregamento adiantado

--- Curso/Program.cs
+++ Curso/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Curso.Data;
 using Microsoft.EntityFrameworkCore;
 using PedidoConsole.Domain;
@@ -9,10 +11,15 @@ namespace PedidoConsole // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            InserirEmMassa();
+            AtualizarDados();
         }
 
-        public static void InserirDados()
+        private static void AtualizarDados()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void InserirDados()
         {
             var db = new ApplicationContext();
 
@@ -42,7 +49,7 @@ namespace PedidoConsole // Note: actual namespace depends on the project name.
             }
         }
 
-        public static void InserirEmMassa()
+        private static void InserirEmMassa()
         {
 
             var produto = new Produto
@@ -66,7 +73,62 @@ namespace PedidoConsole // Note: actual namespace depends on the project name.
 
             db.AddRange(produto, cliente);
             var registros = db.SaveChanges();
+        }
 
+        private static void ConsultarDados(){
+            using var db = new ApplicationContext();
+
+            //consulta rastreada
+            var clientes = db.Set<Cliente>()
+            .Where(c => c.Id > 0)
+            .OrderBy(o => o.Nome)
+            .ToList();
+
+            //consulta nao rastreada
+            var clientesNoTracking = db.Set<Cliente>().AsNoTracking().Where(c => c.Id > 0).ToList();
+
+            foreach(var cliente in clientes){
+                //FIND metodo que verifica na memoria
+                var buscarPorFind = db.Set<Cliente>().Find(cliente.Id);
+            }
+
+        }
+    
+        private static void InserirItemPedido(){
+            using var db = new ApplicationContext();
+
+            var cliente = db.Set<Cliente>().FirstOrDefault();
+            var produto = db.Set<Produto>().FirstOrDefault();
+
+            var pedido = new Pedido{
+                ClienteId = cliente.Id,
+                IniciadoEm = DateTime.Now,
+                FinalizadoEm = DateTime.Now.AddHours(2),
+                Observacao = "teste",
+                Status = Curso.ValueObjects.StatusPedido.Analise,
+                TipoFrete = Curso.ValueObjects.TipoFrete.SemFrete,
+                Itens = new List<PedidoItem>{
+                    new PedidoItem{
+                        ProdutoId = produto.Id,
+                        Desconto = 0,
+                        Quantidade = 1,
+                        Valor = 10
+                    }
+                }
+            };
+
+            db.Set<Pedido>().Add(pedido);
+            db.SaveChanges();
+        }
+
+        private static void ConsultarComCarregamentoAdiantado(){
+            using var db = new ApplicationContext();
+
+            //COnsulta com carregamento adiantado, onde ja carrega todos os subobjetos
+            var pedido = db.Set<Pedido>()
+            .Include(p => p.Itens)
+            .ThenInclude(i=>i.Produto)
+            .ToList();
         }
     }
 }
